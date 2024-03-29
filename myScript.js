ab1e9bb8853e2a280b263c644c913e22fb7bd659
AJ <117821610+aattttt@users.noreply.github.com>
2024-03-29 11:07:57 -0500

    tab script removed myScript.js

--- myScript.js
+++ myScript.js
@@ -1,20 +1,4 @@
-
-
 let map;
-
-function openCity(evt, cityName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
 
 async function initMap() {
   // The location of Bloomington, IL
